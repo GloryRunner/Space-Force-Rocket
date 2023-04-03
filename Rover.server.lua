0cb4f3fadce81b5e03c615dbc3b9395ce2132c76
Glory <89966094+GloryRunner@users.noreply.github.com>
2023-04-03 12:24:12 -0700

    Add files via upload

---
+++ Rover.server.lua
@@ -0,0 +1,28 @@
+local Debris = game:GetService("Debris")
+local RunService = game:GetService("RunService")
+
+local Part = workspace:WaitForChild("Part")
+
+local RAY_DISTANCE = 100
+local BodyPosition = Part.BodyPosition
+
+
+RunService.Heartbeat:Connect(function()
+    local Origin = Part.Position
+    local DirectionCF = CFrame.lookAt(Part.Position + -Part.CFrame.UpVector * RAY_DISTANCE, Part.Position)
+    local RayParams = RaycastParams.new({Part}, Enum.RaycastFilterType.Exclude, true, nil, true)
+    local RayResult = workspace:Raycast(Origin, DirectionCF.Position, RayParams)
+
+    if RayResult then
+        if RayResult.Instance then
+            local SurfaceNorm = RayResult.Normal 
+            local NormToPartPos = Part.Position - SurfaceNorm
+            local LVDotNorm = SurfaceNorm:Dot(NormToPartPos)
+            local Theta = math.deg(math.asin(LVDotNorm / NormToPartPos.Magnitude))
+            Part.Orientation = Vector3.new(-Theta, 0, 0)
+            BodyPosition.Position = Part.Position + CFrame.fromAxisAngle(SurfaceNorm, math.pi / 2).RightVector * 20
+        end
+    else
+        BodyPosition.Position = Part.Position + Part.CFrame.LookVector * 20
+    end
+end)
